f93a139e2bd6a7efd4e0a94f0cc8d578262b7344
Shahriyar <iambabazada@gmail.com>
2023-10-04 22:57:40 +0400

    accepted change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-<<<<<<< HEAD
 import Register from './pages/auth/Register'
-=======
 import Login from './pages/auth/Login'
->>>>>>> feature-login
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Register />
-=======
-       <Login />
->>>>>>> feature-login
+      <Login />
     </div>
   )
 }
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -2,88 +2,80 @@ import React from "react";
 import "../../style/login.css";
 import { useState, useEffect } from "react";
 
-<<<<<<< HEAD
-
-const login = () => {
-    return (
-        <div>login</div>
-    )
-=======
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(true);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(true);
 
-  const handleValidate = (e) => {
-    const login = {
-        username: username,
-        password: password
+    const handleValidate = (e) => {
+        const login = {
+            username: username,
+            password: password
+        }
+        console.log(login);
+
+        setUsername('');
+        setPassword('');
+
+        e.preventDefault();
     }
-    console.log(login);
 
-    setUsername('');
-    setPassword('');
+    useEffect(() => {
+        if (username != "" || username != null) {
+            setError(false)
+        }
+        if (password != "" || password != null) {
+            setError(false)
+        }
+    }, [username, password, error])
 
-    e.preventDefault();
-  }
 
-  useEffect(() => {
-    if(username != "" || username != null){
-        setError(false)
-    }
-    if(password != "" || password != null){
-        setError(false)
-    }
-  }, [username, password, error])
-  
+    return (
+        <div className="container">
+            <div className="img-box">{/* <img src="" alt="" /> */}</div>
+            <div className="login-field">
+                <div className="login-box">
+                    <h4>Welcome Login Page</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <form className="login-form" onSubmit={handleValidate}>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            placeholder="username"
+                            name="name"
+                            id="name"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        {username === "" ? <ErrorMessage>Username is required</ErrorMessage> : ""}
 
-  return (
-    <div className="container">
-      <div className="img-box">{/* <img src="" alt="" /> */}</div>
-      <div className="login-field">
-        <div className="login-box">
-          <h4>Welcome Login Page</h4>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <form className="login-form" onSubmit={handleValidate}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              placeholder="username"
-              name="name"
-              id="name"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {username === "" ? <ErrorMessage>Username is required</ErrorMessage> : ""}
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            placeholder="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        {password === "" ? <ErrorMessage>Password is required</ErrorMessage> : ""}
 
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              placeholder="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {password === "" ? <ErrorMessage>Password is required</ErrorMessage> : ""}  
-            
 
-            <div className="submit-button">
-              <button className="button">Login</button>
+                        <div className="submit-button">
+                            <button className="button">Login</button>
+                        </div>
+                    </form>
+                    <span>
+                        Lorem ipsum dolor sit.? <a href="#">Register</a>
+                    </span>
+                </div>
             </div>
-          </form>
-          <span>
-            Lorem ipsum dolor sit.? <a href="#">Register</a>
-          </span>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-const ErrorMessage = ({children}) => {
+const ErrorMessage = ({ children }) => {
     return <span className="error">{children}</span>
->>>>>>> feature-login
 }
 
 export default Login;
