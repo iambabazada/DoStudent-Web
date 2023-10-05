e318401c4f5ddc0d887f78ea9c08bab6fc9610e2
Rufat01 <rufatmustafayev04@gmail.com>
2023-10-05 12:11:53 +0400

    errors fixed

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState(true);
 
-    const handleValidate = (e) => {
+    const handleValidate = () => {
         const login = {
             username: username,
             password: password
@@ -16,46 +16,32 @@ const Login = () => {
 
         setUsername('');
         setPassword('');
-
-        e.preventDefault();
+        
     }
 
-    if(username !== "" || username != null){
-        setError(false)
-    }
-    if(password !== "" || password != null){
-        setError(false)
-    }
-
-    setUsername('');
-    setPassword('');
     useEffect(() => {
-        if (username != "" || username != null) {
+        if(username !== ""){
             setError(false)
         }
-        if (password != "" || password != null) {
+        if(password !== ""){
             setError(false)
         }
-    }, [username, password, error])
+    }, [username, password])
 
-
-  useEffect(() => {
-    
-    
-  }, [username, password, error])
   
     return (
-        <div className="container">
-            <div className="img-box">{/* <img src="" alt="" /> */}</div>
-            <div className="login-field">
-                <div className="login-box">
+        <div className={styles.container}>
+            <div className={styles.img_box}>{/* <img src="" alt="" /> */}</div>
+            <div className={styles.login_field}>
+                <div className={styles.login_box}>
                     <h4>Welcome Login Page</h4>
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <form className="login-form" onSubmit={handleValidate}>
+                    <form className={styles.login_form} onSubmit={handleValidate}>
                         <label htmlFor="name">Name</label>
                         <input
                             type="text"
                             placeholder="username"
+                            className={styles.name}
                             name="name"
                             id="name"
                             value={username}
@@ -67,6 +53,7 @@ const Login = () => {
                         <input
                             type="password"
                             placeholder="password"
+                            className={styles.password}
                             name="password"
                             id="password"
                             value={password}
@@ -75,8 +62,8 @@ const Login = () => {
                         {password === "" ? <ErrorMessage>Password is required</ErrorMessage> : ""}
 
 
-                        <div className="submit-button">
-                            <button className="button">Login</button>
+                        <div className={styles.submit_button}>
+                            <button className={styles.button}>Login</button>
                         </div>
                     </form>
                     <span>
@@ -90,7 +77,7 @@ const Login = () => {
 
 
 const ErrorMessage = ({ children }) => {
-    return <span className="error">{children}</span>
+    return <span className={styles.error}>{children}</span>
 }
 
 export default Login;
