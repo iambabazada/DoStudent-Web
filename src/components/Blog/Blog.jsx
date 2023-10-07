ec3cffc5a203665ea7ca98fefd502f31ddab447b
Rufat01 <rufatmustafayev04@gmail.com>
2023-10-08 01:15:18 +0400

    fake data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,37 +1,34 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/components/Blog.module.css";
-import avatar from '../../assets/images/avatar.png'
+import avatar from "../../assets/images/avatar.png";
 import { FaUser, FaCalendar } from "react-icons/fa";
 
-const Blog = ({ users }) => {
-
-
+const Blog = ({ data }) => {
   return (
     <div className={styles.blog_container}>
-      {users?.slice(0, 3).map((user, index) => (
-        <BlogItem key={index} user={user} />
+      {data?.map((data) => (
+        <BlogItem user={data} key={data.id} />
       ))}
+      
     </div>
   );
 };
 
 const BlogItem = ({ user }) => {
-  return <div className={styles.blog_item}>
-    <div className={styles.avatar}>
-      <img src={avatar} alt="" />
-    </div>
-    <div className={styles.details_container}>
-      <h4>{user?.email}</h4>
-      <div className={styles.details}>
-        <span>
-          {user?.name}
-        </span>
-        <span>
-          {user?.website}
-        </span>
+  return (
+    <div className={styles.blog_item}>
+      <div className={styles.avatar}>
+        <img src={avatar} alt="" />
+      </div>
+      <div className={styles.details_container}>
+        <h4>{user.title}</h4>
+        <div className={styles.details}>
+          <span>{user.content}</span>
+          <span>{user?.website}</span>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Blog;
--- src/pages/home/Hero/Hero.jsx
+++ src/pages/home/Hero/Hero.jsx
@@ -1,163 +1,232 @@
-import React, { useEffect, useState } from 'react'
-import Card from '../../../components/Card/Card'
-import Blog from '../../../components/Blog/Blog'
+import React, { useEffect, useState } from "react";
+import Card from "../../../components/Card/Card";
+import Blog from "../../../components/Blog/Blog";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import styles from './Hero.module.css'
-import LeftArrow from '../../../assets/icons/left-arrow.svg'
+import styles from "./Hero.module.css";
+import LeftArrow from "../../../assets/icons/left-arrow.svg";
 
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <img
-            src={LeftArrow}
-            onClick={onClick}
-            alt=""
-            className={className}
-            style={{ ...style, display: "block", color: "green", rotate: "180deg", width: "60px", height: "60px", borderRadius: "100%", }}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <img
+      src={LeftArrow}
+      onClick={onClick}
+      alt=""
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        color: "green",
+        rotate: "180deg",
+        width: "60px",
+        height: "60px",
+        borderRadius: "100%",
+      }}
+    />
+  );
 }
 
 function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <img
-            src={LeftArrow}
-            onClick={onClick}
-            alt=""
-            className={className}
-            style={{ ...style, display: "block", color: "green", rotate: "360deg", width: "60px", height: "60px", borderRadius: "100%", }}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <img
+      src={LeftArrow}
+      onClick={onClick}
+      alt=""
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        color: "green",
+        rotate: "360deg",
+        width: "60px",
+        height: "60px",
+        borderRadius: "100%",
+      }}
+    />
+  );
 }
 
 const Hero = () => {
-    const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-        centerPadding: "0px",
-        slidesToShow: 3,
-        autoplay: false,
-        speed: 500,
-        autoplaySpeed: 5000,
-        cssEase: "linear",
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
-    };
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "0px",
+    slidesToShow: 3,
+    autoplay: false,
+    speed: 500,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
 
-    const [user, setUser] = useState()
+  const [user, setUser] = useState();
 
-    useEffect(() => {
-        async function getUser() {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users')
-            const data = await response.json()
-            // console.log(data);
-            setUser(data)
-        }
-        getUser()
-    }, [])
+  useEffect(() => {
+    async function getUser() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      // console.log(data);
+      setUser(data);
+    }
+    getUser();
+  }, []);
 
-    // const userData = [
-    //     {
-    //         id: 1,
-    //         title: "title",
-    //         content: "content"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "title 2",
-    //         content: "content 2"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "title 3",
-    //         content: "content 3"
-    //     },
-    //     {
-    //         id: 1,
-    //         title: "title",
-    //         content: "content"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "title 2",
-    //         content: "content 2"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "title 3",
-    //         content: "content 3"
-    //     },
-    // ]
+  const userData = [
+    {
+      id: 1,
+      title: "title1",
+      content: "content",
+    },
+    {
+      id: 2,
+      title: "title 2",
+      content: "content 2",
+    },
+    {
+      id: 3,
+      title: "title 3",
+      content: "content 3",
+    },
+    {
+      id: 4,
+      title: "title4",
+      content: "content",
+    },
+    {
+      id: 5,
+      title: "title 5",
+      content: "content 2",
+    },
+    {
+      id: 6,
+      title: "title 6",
+      content: "content 3",
+    },
+    {
+      id: 7,
+      title: "title 7",
+      content: "content",
+    },
+    {
+      id: 8,
+      title: "title 8",
+      content: "content 2",
+    },
+    {
+      id: 9,
+      title: "title 9",
+      content: "content 3",
+    },
+    {
+      id: 10,
+      title: "title 10",
+      content: "content",
+    },
+    {
+      id: 11,
+      title: "title 11",
+      content: "content 2",
+    },
+    {
+      id: 12,
+      title: "title 12",
+      content: "content 3",
+    },
+  ];
 
-    // const card = [
-    //     {
-    //         id: 1,
-    //         price: 500,
-    //         location: "baki OYU",
-    //         rooms: "3 otaqliii"
-    //     },
-    //     {
-    //         id: 1,
-    //         price: 600,
-    //         location: "Samaxi OYU",
-    //         rooms: "3 otaqliii"
-    //     },
-    //     {
-    //         id: 1,
-    //         price: 100,
-    //         location: "Lenkeran OYU",
-    //         rooms: "3 otaqliii"
-    //     },
-    // ]
+  const card = [
+    {
+      id: 1,
+      price: 500,
+      location: "baki OYU",
+      rooms: "3 otaqliii",
+    },
+    {
+      id: 1,
+      price: 600,
+      location: "Samaxi OYU",
+      rooms: "3 otaqliii",
+    },
+    {
+      id: 1,
+      price: 100,
+      location: "Lenkeran OYU",
+      rooms: "3 otaqliii",
+    },
+  ];
 
-    // const books = [
-    //     {
-    //         id: 1,
-    //         title: "riyaziyyat",
-    //         desc: "cdefvrgvbgrb"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Informatika",
-    //         desc: "cdefvrgvbgrb"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "Ana dili",
-    //         desc: "cdefvrgvbgrb"
-    //     },
-    // ]
+  const books = [
+    {
+      id: 1,
+      title: "riyaziyyat",
+      desc: "cdefvrgvbgrb",
+    },
+    {
+      id: 2,
+      title: "Informatika",
+      desc: "cdefvrgvbgrb",
+    },
+    {
+      id: 3,
+      title: "Ana dili",
+      desc: "cdefvrgvbgrb",
+    },
+    {
+      id: 3,
+      title: "Ana dili",
+      desc: "cdefvrgvbgrb",
+    },
+    {
+      id: 3,
+      title: "Ana dili",
+      desc: "cdefvrgvbgrb",
+    },
+    {
+      id: 3,
+      title: "Ana dili",
+      desc: "cdefvrgvbgrb",
+    },
+  ];
+
+  const mixData = [...books, ...userData, ...card];
+
+  const roomsArray = mixData.filter((item) => item.rooms);
+  const booksArray = mixData.filter((item) => item.desc);
+  const usersArray = mixData.filter((item) => item.content);
+
+  let concattedArr = [];
+
+  let arrayIndex = 0;
+  let arrayValue = 0;
+  while (arrayIndex < usersArray.length) {
+    arrayValue += 3;
+    concattedArr.push(
+      usersArray.slice(arrayIndex, arrayValue).map((arr) => arr)
+    );
+    arrayIndex = arrayValue;
+  }
 
 
+  return (
+    <div className={styles.hero}>
+      <Slider {...settings}>
+        {concattedArr.map((arr, i) => {
+          if (i % 2 !== 0) {
+            return <Blog data={arr}/>;
+          }else{
+            return <Card />
+          }
+        })}
+      </Slider>
+    </div>
+  );
+};
 
-    // const mixData = [
-    //     ...books, ...userData, ...card
-    // ]
-
-    // const handleSubmit = () => {
-    //     const roomsArray = mixData.filter(item => item.rooms !== undefined);
-    //     console.log(roomsArray);
-    // };
-
-
-    return (
-        <div className={styles.hero}>
-
-            <Slider {...settings}>
-                <Card />
-                <Blog users={user} />
-                <Card />
-                <Blog users={user} />
-                <Card />
-            </Slider>
-        </div>
-    )
-}
-
-
-
-export default Hero
+export default Hero;
